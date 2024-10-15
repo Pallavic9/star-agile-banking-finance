ecf03bb3417dc278483b2773a013955385c86954
Pallavi Chaudhari <79018394+Pallavic9@users.noreply.github.com>
2024-10-15 15:19:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,20 @@
+#Generate an SSH key pair
+resource "tls_private_key" "ec2_key_pair" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# create key
+resource "aws_key_pair" "key_pair" {
+  key_name   = "my-key"
+  public_key = tls_private_key.ec2_key_pair.public_key_openssh
+}
 
 resource "aws_instance" "test-server" {
   ami = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"
-  key_name = "keypair2"
-  connection {
-     type = "ssh"
-     user = "ubuntu"
-     private_key = file("./keypair2.pem")
-     host = self.public_ip
-     }
+  key_name = aws_key_pair.key_pair.key_name
+
   provisioner "remote-exec" {
      inline = ["echo 'wait to start the instance' "]
   }
