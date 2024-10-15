05fad96b1c711722c19fa456e1fcf211da92f538
Pallavi Chaudhari <79018394+Pallavic9@users.noreply.github.com>
2024-10-15 20:56:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,8 +45,7 @@ resource "aws_instance" "test-server" {
   provisioner "local-exec" {
      command = "echo ${aws_instance.test-server.public_ip} > inventory"
      }
- provisioned "file-exec" {
-      
+     
   provisioner "local-exec" {
      command = "ansible-playbook /var/lib/jenkins/workspace/finance-project/ansible-playbook.yml"
      }
