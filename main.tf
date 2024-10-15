2b04eceb2e895714a90ea8265dcdc72bbdbd3c44
Pallavi Chaudhari <79018394+Pallavic9@users.noreply.github.com>
2024-10-15 16:28:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,23 @@
-resource "aws_key_pair" "my_key" {
-  key_name   = "my-key"
-  public_key = file(".ssh/id_rsa.pub")  # Make sure to provide the path to your public key file
+resource "tls_private_key" "my_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
 }
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "generated-key"
+  public_key = tls_private_key.my_key.public_key_openssh
+}
+
 resource "aws_instance" "test-server" {
   ami = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"
-  key_name = aws_key_pair.my_key.key_name
+  key_name = aws_key_pair.generated_key.key_name
 
   provisioner "remote-exec" {
       connection {
       type        = "ssh"
       user        = "ubuntu"             # Use the default user for your AMI
-      private_key = file("~/.ssh/id_rsa")  # Path to the private key that matches the public key
+      private_key = tls_private_key.my_key.private_key_pem  # Path to the private key that matches the public key
       host        = self.public_ip         # Use the instance’s public IP
     }
      inline = ["echo 'wait to start the instance' "]
