8fb1dcd5a1accff7b40400324be7b2877edf3f50
Pallavi Chaudhari <79018394+Pallavic9@users.noreply.github.com>
2024-10-15 13:27:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "test-server" {
   ami = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"
   key_name = "keypair2"
-  vpc_security_group_ids = ["sg-0624c6a8b6420566d"]
   connection {
      type = "ssh"
      user = "ubuntu"
